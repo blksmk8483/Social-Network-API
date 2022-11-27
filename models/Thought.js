eccce275c352f7b5a0f1b9f86c0b1876c76040cd
Brandon Knight <knightbrandon1984@gmail.com>
2022-11-27 15:27:19 -0600

    added the Thought model

---
+++ models/Thought.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            max_length: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reaction],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+    }
+);
+
+thoughtSchema
+.virtual('reactionCount')
+.get(function () {
+    return `REACTION COUNT: ${this.reactions.length}`
+});
+
+const Thought = model('thought', userSchema);
+
+module.exports = Thought;
