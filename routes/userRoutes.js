2819409e3edcca63a6a38242333d9eade7efe6ad
Brandon Knight <knightbrandon1984@gmail.com>
2022-11-27 19:21:34 -0600

    added USER routes

---
+++ routes/userRoutes.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const {
+    getAllUsers,
+    getSingleUser,
+    createNewUser,
+    updateSingleUser,
+    deleteUser,
+    createUserFriend,
+    deleteUserFriend,
+
+} = require('../../controllers/userController.js');
+
+// -------- api/ --------
+router.route('/')
+.get(getAllUsers)
+.post(createNewUser);
+
+// -------- api/users/:userID --------
+router.route('/:userId')
+.get(getSingleUser)
+.put(updateSingleUser)
+.delete(deleteUser);
+
+// -------- api/users/:userId/friends/:friendId --------
+router.route('/api/users/:userId/friends/:friendId')
+.post(createUserFriend)
+.delete(deleteUserFriend);
+
+module.exports = router;
